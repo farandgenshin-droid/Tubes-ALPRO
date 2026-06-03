8c11b1149f947479609aa7903fd90121833d671c
Aulia Adzkia Rahmah <2510514029@mahasiswa.upnvj.ac.id>
2026-06-03 08:45:10 +0700

    Update LITE-O MAIN.c

--- LITE-O MAIN.c
+++ LITE-O MAIN.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* ===================== TIPE DATA ===================== */
+
+typedef struct {
+    int x;
+    int y;
+    char nama[50];
+    int skor;
+} Hadiah;
+
+typedef struct {
+    int x;
+    int y;
+} Gerak;
+
+/* ===================== MESIN WAKTU ===================== */
+
+void tahan(float x) {
+    time_t start;
+    time_t current;
+    time(&start);
+    do
+        time(&current);
+    while (difftime(current, start) < x);
+}
+
+/* ===================== VARIABEL GLOBAL ===================== */
+
+int panjang, lebar;
+
+/* ===================== PROGRAM UTAMA ===================== */
 
 int main() {
-    // Your code goes here
-    printf("Hello, World!\n");
-    
+    printf("Selamat Datang di Program Lite-O\n");
+    printf("Masukan Panjang dan Lebar (pisahkan dengan spasi) : ");
+    scanf("%d %d", &panjang, &lebar);
+
+    /* TODO: tampilkan menu utama */
+
     return 0;
 }
