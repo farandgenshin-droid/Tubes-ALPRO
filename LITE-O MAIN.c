411c8d44e6814fb4770da75eb3d02cd1a79cd2fe
narendrabhaskoro <2510514022@mahasiswa.upnvj.ac.id>
2026-06-03 09:11:19 +0700

    wwww
    w

--- LITE-O MAIN.c
+++ LITE-O MAIN.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main() {
+<<<<<<< Updated upstream
     // Your code goes here
     printf("Hello, World!\n");
+=======
+    printf("Selamat Datang di Program Lite-O\n");
+    printf("Masukan Panjang dan Lebar (pisahkan dengan spasi) : ");
+    scanf("%d %d", &panjang, &lebar);
+
+    printf("kia macbook");
+    /* TODO: tampilkan menu utama */
+>>>>>>> Stashed changes
 
     printf("p anji");
     
