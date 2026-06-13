8c871cc976cff2a15982ada441650a35ac0b62d0
farandgenshin-droid <farandgenshin@gmail.com>
2026-06-13 22:32:21 +0700

    Update LITEOMAIN.c

--- LITEOMAIN.c
+++ LITEOMAIN.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
 /* TIPE DATA */
 
 typedef struct {
@@ -32,13 +31,95 @@ void tahan(float x) {
 
 
 int panjang, lebar;
+int hadiah;
+int i,j;
+int tambahataurewrite;
 int menu;
 int x,y;
 Gerak g;
 Hadiah h;
 char player = '0'; 
 int skor = 0;
+char move; 
+char bentukhadiah = '*';
+char map;
+int a,b;
+Hadiah dataHadiah[100];
+int jumlahHadiah;
 
+
+void writehadiah() {
+      FILE *Thadiah= fopen("thadiah.txt", "w") ;
+            printf("Jumlah hadiah : ");
+            scanf("%d", &hadiah);
+            for(i = 0; i < hadiah; i++){
+            printf("Input hadiah ke-%d:\n", i + 1);
+             printf("x, y, nama, skor1 : ");
+             scanf("%d %d %s %d", &h.x, &h.y, h.nama, &h.skor);
+             fprintf(Thadiah, "%d %d %s %d\n", h.x, h.y, h.nama, h.skor);
+        }
+            fprintf(Thadiah, "###");
+            fclose(Thadiah); 
+}
+
+void readhadiah () {
+    Hadiah temp;
+    jumlahHadiah = 0;
+    FILE *Thadiah = fopen("thadiah.txt", "r");
+    
+    if (Thadiah == NULL)
+    {
+        printf("Gagal membaca file thadiah.txt\n");
+    }
+    else
+    {
+        while (fscanf(Thadiah, "%d %d %s %d", &dataHadiah[jumlahHadiah].x, &dataHadiah[jumlahHadiah].y, dataHadiah[jumlahHadiah].nama, &dataHadiah[jumlahHadiah].skor) == 4)
+        {
+            jumlahHadiah++;
+        }
+        for (i = 0; i < jumlahHadiah - 1; i++)
+        {
+            for (j = 0; j < jumlahHadiah - 1 - i; j++)
+            {
+                if (dataHadiah[j].y > dataHadiah[j + 1].y)
+                {
+                    temp = dataHadiah[j];
+                    dataHadiah[j] = dataHadiah[j + 1];
+                    dataHadiah[j + 1] = temp;
+                }
+            }
+        }
+        fclose(Thadiah);
+        Thadiah = fopen("thadiah.txt", "w");
+        printf("\nData hadiah saat ini:\n");
+
+        for (i = 0; i < jumlahHadiah; i++)
+        {
+            fprintf(Thadiah, "%d %d %s %d\n", dataHadiah[i].x, dataHadiah[i].y, dataHadiah[i].nama, dataHadiah[i].skor);
+            printf(" Posisi (X:%d, Y:%d) |  nama: %s  | Skor: %d\n", dataHadiah[i].x, dataHadiah[i].y, dataHadiah[i].nama, dataHadiah[i].skor);
+        }
+        
+        fprintf(Thadiah, "###"); 
+        
+        fclose(Thadiah);
+    }
+}
+
+void appendhadiah()
+{
+    FILE *Thadiah = fopen("thadiah.txt", "a");
+    printf("silahkan tambahkan data: \n");
+    printf("Jumlah hadiah : ");
+    scanf("%d", &hadiah);
+    for (i = 0; i < hadiah; i++)
+    {
+        printf("Input hadiah ke-%d:\n", i + 1);
+        printf("x, y, nama, skor : ");
+        scanf("%d %d %s %d", &dataHadiah[jumlahHadiah].x, &dataHadiah[jumlahHadiah].y, dataHadiah[jumlahHadiah].nama, &dataHadiah[jumlahHadiah].skor);
+        jumlahHadiah++; 
+    }
+    fclose(Thadiah);
+}
 
 /* PROGRAM UTAMA */
 
@@ -47,7 +128,7 @@ int main() {
     printf("Selamat Datang di Program Lite-O\n");
     printf("Masukan Panjang dan Lebar (pisahkan dengan spasi) : ");
     scanf("%d %d", &panjang, &lebar);
-     char map[panjang][lebar];
+     char map[panjang+1][lebar+1];
 
  while(1) {
         printf("Menu:\n");
@@ -59,66 +140,34 @@ int main() {
 
         scanf(" %d", &menu);
 
-        if(menu == 1) {
-             int hadiah, i;
-            FILE *TulisHadiah = fopen("thadiah.txt", "w") ;
-            printf("Jumlah hadiah : ");
-            scanf("%d", &hadiah);
-            for(i = 0; i < hadiah; i++){
-            printf("Input hadiah yang diinginkan : ");
-            scanf("%d %d %s %d", &h.x, &h.y, h.nama, &h.skor);
-            fprintf(TulisHadiah, "%d %d %s %d\n", h.x, h.y, h.nama, h.skor);
-        }
-            fprintf(TulisHadiah, "###");
-            fclose(TulisHadiah); 
+       if(menu == 1) { // ini gw izin jadiin void yak, sama izin sortinganya gw ubah ke bubble, jujur gw ga paham ma sortingan lu aul wkkwkw
+            printf("Ketik 1 untuk tambah hadiah atau angka berapapun untuk rewrite hadiah: ");
+            scanf("%d", &tambahataurewrite);
             
-            /* TAMBAHAN SORTING */
-            Hadiah dataHadiah[100];
-            Hadiah temp;
-            int j;
-            int jumlahHadiah = 0;
+            if (tambahataurewrite == 1) {
+                readhadiah();
+                appendhadiah();
+                FILE *Thadiah= fopen("thadiah.txt", "w");
 
-            TulisHadiah = fopen("thadiah.txt", "r");
+                for(i = 0; i < jumlahHadiah; i++) {
+                    fprintf(Thadiah, "%d %d %s %d\n", dataHadiah[i].x, dataHadiah[i].y, dataHadiah[i].nama, dataHadiah[i].skor);
+                }
+                fprintf(Thadiah, "###");
+                fclose(Thadiah);
+            } 
+            else {
+                writehadiah();
+                readhadiah();
+            FILE *Thadiah= fopen("thadiah.txt", "w");
 
-            while(fscanf(TulisHadiah,
-                 "%d %d %s %d",
-                 &dataHadiah[jumlahHadiah].x,
-                 &dataHadiah[jumlahHadiah].y,
-                 dataHadiah[jumlahHadiah].nama,
-                 &dataHadiah[jumlahHadiah].skor) == 4)
-    {
-            jumlahHadiah++;
-    }
-    fclose(TulisHadiah);
-
-    for(i = 0; i < jumlahHadiah - 1; i++)
-    {
-        for(j = i + 1; j < jumlahHadiah; j++)
-        {
-            if(dataHadiah[i].y > dataHadiah[j].y ||
-              (dataHadiah[i].y == dataHadiah[j].y &&
-               dataHadiah[i].x > dataHadiah[j].x))
-            {
-               temp = dataHadiah[i];
-               dataHadiah[i] = dataHadiah[j];
-               dataHadiah[j] = temp;
+            for(i = 0; i < jumlahHadiah; i++) {
+                 fprintf(Thadiah, "%d %d %s %d\n", dataHadiah[i].x, dataHadiah[i].y, dataHadiah[i].nama, dataHadiah[i].skor);
             }
-        }
-    }
-    TulisHadiah = fopen("thadiah.txt", "w");
-
-    for(i = 0; i < jumlahHadiah; i++)
-    {
-        fprintf(TulisHadiah,
-                "%d %d %s %d\n",
-                dataHadiah[i].x,
-                dataHadiah[i].y,
-                dataHadiah[i].nama,
-                dataHadiah[i].skor);
-    }
-        fprintf(TulisHadiah, "###");
-        fclose(TulisHadiah);
-        }
+            fprintf(Thadiah, "###");
+            fclose(Thadiah);
+            }
+        } 
+        
         else if(menu == 2) {
             int n, i;
             FILE *gerak = fopen("tgerak.txt", "w");
@@ -132,7 +181,7 @@ int main() {
             while (g.x <= 0 || g.y <= 0)
         {
            printf("ga bisa 0 dan mines");
-        return 0;
+        break;
         }
         
         fprintf(gerak, "%d %d\n", g.x, g.y);
@@ -159,7 +208,20 @@ int main() {
         }
     }
 }
+
+FILE *Thadiah = fopen("thadiah.txt", "r");
+while (fscanf(Thadiah, "%d %d %s %d", &h.x, &h.y, h.nama, &h.skor) == 4) {
+    map[h.y][h.x] = bentukhadiah;
+}
+fclose(Thadiah);
+
+
+FILE *gerak = fopen("tgerak.txt", "r");
+while (fscanf(gerak,"%d %d", &g.x, &g.y) == 2 ){
 map[g.y][g.x] = player;
+}fclose(gerak);
+
+
 
     for (a = 0; a < panjang; a++) {
         for (b = 0; b < lebar; b++) {
@@ -168,7 +230,8 @@ map[g.y][g.x] = player;
     printf("\n");
 }
     printf("\nSkor O : %d\n", skor);    
-   }   else if(menu == 4) {
+   }  
+    else if(menu == 4) {
             printf("\nTerima kasih telah bermain!\n");
             break;
         }
